f709dbb9b3c9901f4d164e1819c28d108c50dd77
Crystal <crystal120599@gmail.com>
2023-11-20 21:44:56 -0800

    create associations

--- models/index.js
+++ models/index.js
@@ -4,6 +4,17 @@ const Post = require("./Post");
 const Profile = require("./Profile");
 const User = require("./User");
 
+User.hasOne(Profile);
+Profile.belongsTo(User);
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+User.belongsToMany(Like, {through: "User-Like"});
+Like.belongsToMany(User, {through: "User-Like"})
 module.exports = {
     Comment,
     Like,
