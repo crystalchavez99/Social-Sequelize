097948613c9a0c97d030102c7738b4e5895f1fef
Crystal <crystal120599@gmail.com>
2023-11-20 21:12:21 -0800

    Create sequelize connection to db

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,14 @@
+const { Sequelize } = require("sequelize")
 
+let db = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+})
 
 
 
 
 module.exports = {
     db,
-    
+
 }
