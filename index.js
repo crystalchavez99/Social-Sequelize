013b8e34acab18946cf4c746061ed24cd8f170c3
Crystal <crystal120599@gmail.com>
2023-11-21 08:57:35 -0800

    place association in correct index

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const { Comment, Like, Post, Profile, User } = require("./models/index");
 
 
 
+User.hasOne(Profile);
+Profile.belongsTo(User);
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+User.belongsToMany(Like, {through: "User-Like"});
+Like.belongsToMany(User, {through: "User-Like"})
 
 
 module.exports = {
